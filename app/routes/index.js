d7df527e45285eeb42926314195d665857513a46
trevorcrupi <trevor.crupi27@gmail.com>
2017-05-02 16:28:04 -0400

    Initial Commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,10 +7,11 @@ const AuthRoutes = require('./oauth');
 const UserRoutes = require('./users');
 const developers = require('./users/developers');
 const nonprofits = require('./users/nonprofits');
-const profile    = require('./profile');
 const JwtRoutes  = require('./token');
 const userInt    = require('../handlers/User');
 const path       = require('path');
+const projects   = require('./projects');
+const matches    = require('./matches');
 
 // Telling express what route goes to this greeting.
 routes.use('/api/greeting', greetings);
@@ -19,9 +20,11 @@ routes.use('/api/users', UserRoutes);
 routes.use('/api/developers', developers);
 routes.use('/api/nonprofits', nonprofits);
 routes.use('/oauth', AuthRoutes);
-routes.use('/', profile);
+
 // The actual generating of the token should be done when a user logs in or something.
 routes.use('/api/token', JwtRoutes);
+routes.use('/api/projects', projects);
+routes.use('/api/matches', matches);
 
 // res = response, req = request.
 // More routing, telling express what to send back.
@@ -29,57 +32,6 @@ routes.get('/api', (req, res) => {
   res.status(200).json({ message: 'Connection Made!' });
 });
 
-routes.get('/findPath', (req, res) => {
-  const u = new userInt(req);
-  u.getUserAttributes(['interests'], u.getSessUser('_id'),  function(err, interests) {
-    if(interests['interests'] && interests['interests'] !== null && interests['interests'].length > 0) {
-      res.redirect('/');
-    } else {
-      res.redirect('/register/step.2');
-    }
-  });
-});
-
-routes.get('/:user', (req, res) => {
-  var getData = new userInt(req);
-  getData.getUrl(req.params.user, function(err, users){
-    if(err || users.length > 1) {
-      console.log(users);
-      res.status(200).send(err+'Failed');
-      return;
-    }
-    var user = users[0];
-    if(!user) {
-      res.status(200).send('No username exists');
-      return;
-    }
-    getData.getProfile(user.userId, function(err, user) {
-      console.log(user);
-      console.log(user.accountType);
-      if(user.user.accountType === false) {
-        res.render('secure/profile-dev.twig', {
-          "profile": user
-        });
-      } else {
-        res.render('secure/profile-nonprofit.twig', {
-          "profile": user
-        });
-      }
-    });
-  });
-});
-
-routes.get('/', (req, res) => {
-  if(new userInt(req).isSignedIn() === false){
-    res.render("public/index.twig", {
-      "title": "Welcome to Communicode"
-    })
-  } else {
-    res.render("secure/dashboard.twig", {
-      "title": "Welcome to Communicode"
-    })
-  }
-});
 
 // Exporting it.
 module.exports = routes;
